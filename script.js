85c266ce85d6f294e44ff3110df0b8ce32f395cd
Ethan Gill <1ethangill1@gmail.com>
2023-06-11 14:01:13 -0700

    finally got the API info to dispaly

--- script.js
+++ script.js
@@ -1,43 +1,54 @@
 var searchButton = document.getElementById('search');
 var cityHistory = [];
-var cityWeather = [];
 
 function displayCityWeather(weatherData) {
-    var cityNameEl = document.getElementById('cityName');
-    var tempEl = document.getElementById('temperature');
-    var infoEl = document.getElementById('description');
     var weatherContainer = document.getElementById('weatherContainer');
-    
-    cityNameEl.textContent = weatherData.name;
-    tempEl.textContent = weatherData.main.temp;
-    infoEl.textContent = weatherData.weather[0].description; 
+    weatherContainer.innerHTML = '';
 
-    cityNameEl.textContent = 'City: ' + weatherData.name;
-    tempEl.textContent = 'Temperature: ' + weatherData.ain.temp;
-    infoEl.textContent = 'Forcast: ' + weatherData.weather[0].description;
+    if (weatherData && weatherData.name && weatherData.main && weatherData.main.temp && weatherData.weather && weatherData.weather[0] && weatherData.weather[0].description) {
+        var cityNameEl = document.createElement('div');
+        cityNameEl.textContent = 'City: ' + weatherData.name;
 
-    weatherContainer.style.border = '1px solid black';
-    weatherContainer.style.padding = '10px';
-    weatherContainer.style.backgroundColor = 'lightgray';
+        var tempEl = document.createElement('div');
+        tempEl.textContent = 'Temperature: ' + weatherData.main.temp;
+
+        var infoEl = document.createElement('div');
+        infoEl.textContent = 'Forecast: ' + weatherData.weather[0].description;
+
+        weatherContainer.appendChild(cityNameEl);
+        weatherContainer.appendChild(tempEl);
+        weatherContainer.appendChild(infoEl);
+
+        weatherContainer.style.border = '1px solid black';
+        weatherContainer.style.padding = '10px';
+        weatherContainer.style.backgroundColor = 'lightgray';
+    } else {
+        weatherContainer.textContent = 'Error retrieving weather data';
+        weatherContainer.style.border = 'none';
+        weatherContainer.style.padding = '0';
+        weatherContainer.style.backgroundColor = 'transparent';
     }
+}
 
-searchButton.addEventListener('click', function(event) {
+searchButton.addEventListener('click', function (event) {
     var cityData = document.getElementById('input1').value;
     event.preventDefault();
 
     if (cityData) {
-        var apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityData + '&appid=69d0bc2f7af62a8be51de0f9dcf280b5';
-        
+        var apiURL =
+            'https://api.openweathermap.org/data/2.5/weather?q=' +
+            cityData +
+            '&appid=69d0bc2f7af62a8be51de0f9dcf280b5';
+
         fetch(apiURL)
             .then(response => response.json())
             .then(data => {
                 console.log('weather data: ', data);
                 displayCityWeather(data);
-                cityHistoryContainer.style.border = 'none';
-                
             })
             .catch(error => {
                 console.log('Error: ', error);
+                displayCityWeather(null); // Show error message
             });
     } else {
         console.log('Please enter a city');
@@ -47,9 +58,8 @@ searchButton.addEventListener('click', function(event) {
 
     cityHistory.push({
         name: name,
-       
     });
-    
+
     displayCityHistory();
 });
 
@@ -59,27 +69,25 @@ function displayCityHistory() {
 
     var ul = document.createElement('ul');
 
-
     for (var i = 0; i < cityHistory.length; i++) {
         var cityData = cityHistory[i];
         var li = document.createElement('li');
         li.textContent = 'Name: ' + cityData.name;
-            
-    ul.appendChild(li);
 
+        ul.appendChild(li);
     }
+
     cityHistoryContainer.appendChild(ul);
 }
-
 
 searchButton.style.borderRadius = '10px';
 searchButton.style.color = 'white';
 searchButton.style.backgroundColor = 'lightblue';
 
-var cityHistoryContainer = document.getElementById('cityHistoryContainer') 
-    cityHistoryContainer.style.color = 'black';
-    cityHistoryContainer.style.fontSize = '20px';
-    cityHistoryContainer.style.backgroundColor = 'grey';
+var cityHistoryContainer = document.getElementById('cityHistoryContainer');
+cityHistoryContainer.style.color = 'black';
+cityHistoryContainer.style.fontSize = '20px';
+cityHistoryContainer.style.backgroundColor = 'grey';
 
 var cityNameEl = document.getElementById('cityName');
 var tempEl = document.getElementById('temperature');
